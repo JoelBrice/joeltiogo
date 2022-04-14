1eec2b2b8d5785924ac2b7c1a03bb3ca31b924c7
Joel Tiogo <tiogojoel@gmail.com>
2022-04-14 23:58:57 +0200

    basic description added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,19 @@ function App() {
           <div className='col'>
 
             <h1> Welcome to my Personal website!</h1>
+            <br />
+            <h3> I am the CEO & Lead engineer at
+              <a href="https://www.expotechindustries.com"> Expotech Industries</a></h3>
+            <p>
+              <p> I have a very diverse background going from Electrical Engineering to Software Engineering. This background gives me an excellent perspective on solving complex problems.
+                What if I told you that I have studied for 3 degrees first in <em> Physics </em> in Cameroon where I am from and I had to leave to South Africa to pursue my Engineering studies just after my first year in Physics;
+                I got to South Africa late so started a degree with a long distance university (Univeersity of South Africa) in <b>Applied Mathematics & computer science. </b> before pursuing a bachelor of Technology in <b> Electrical Engineering</b></p>
+              <p> Since you are here, you can check out my <b><a className='cv' href='https://docs.google.com/document/d/1BsLlXRjWIyOuWyiV160slcSjBAcXMZRVJi0LagYobzU/edit?usp=sharing'>Curriculum vitae</a> to learn more about my career history</b></p>
+              If my profile interests you for any opportunity, you can <a className='mail' href='mailto:joel.tiogo@xpotechindustries.com'>email me</a>
+            </p>
           </div>
           <div className='col'>
-            <img src={photo} />
+            <img src={photo} className="profile-pic" />
           </div>
         </div>
       </div>
