04a8c891cb5e1b0ded89b575388a8691d64b5f42
Joel Tiogo <tiogojoel@gmail.com>
2023-10-17 23:29:50 +0200

    about content changes

--- src/components/About.js
+++ src/components/About.js
@@ -18,15 +18,16 @@ function About()
                 alt="About Me"
               />
             </div>
-            <div className="mt-5 sm:mt-0">
+            <div className="mt-5 sm:mt-0 justify-content">
               <p className="mt-3 max-w-2xl text-xl text-gray-500">
-              I'm a Business Development Manager with an electrical engineering and software development background. Passionate about leveraging technology for business value and sustainability, with a focus on ESG.
+              I am a seasoned professional with a unique mix of qualifications and experiences, currently spearheading New Business Development at Syenah. From earning a degree in Electrical Engineering to gaining practical experience in the solar energy industry and the software sector, my journey has been rich and diverse.</p>
+              <p className="mt-3 max-w-2xl text-xl text-gray-500">
+              Building on the technical expertise acquired during my academic and professional pursuits, I delved into the software industry, gaining invaluable skills and insights. My entrepreneurial drive led me to establish my own tech start-up, further deepening my understanding of the interplay between technology and business value.</p>
+              <p className="mt-3 max-w-2xl text-xl text-gray-500">
+              Now at Syenah, I fuse my technical know-how with ESG principles, performing diverse roles that encompass data sourcing, forming strategic alliances, and closely working with ESG taxonomies and frameworks. This balanced approach, underpinned by rigorous research, amplifies our platform's capabilities, performance, and commitment to ESG principles.</p>
+              <p className="mt-3 max-w-2xl text-xl text-gray-500">
+              With a deep-seated interest in leadership roles, my aim is to position ESG considerations at the heart of business operations. I am passionate about the synergy between Fourth Industrial Revolution (4IR) technologies, such as AI, and sustainable business growth. My robust networking abilities and social skills, coupled with a commitment to exploring the future of technology, are key drivers in my mission to help businesses navigate the path towards sustainable success.
               </p>
-              <p className="mt-3 max-w-2xl text-xl text-gray-500">
-              After completing my electrical engineering degree, I gained valuable experience as a software developer and founded my own startup. This journey deepened my understanding of technology's role in solving real-world problems and driving business value.
-              </p>
-              <p className="mt-3 max-w-2xl text-xl text-gray-500">
-              In my current role, I facilitate partnerships that drive growth and innovation. I'm dedicated to using technology to create a better world, believing that combining Good ESG practices with advanced technology will shape a brighter future.</p>
             </div>
           </div>
         </div>
